d887db11d867532c80ba795833fb8e97d3f0ff31
Amal pk <amalashok7902@gmail.com>
2022-12-13 13:11:38 +0530

    images

--- lib/constants/images/images.dart
+++ lib/constants/images/images.dart
@@ -1,5 +1,5 @@
 class ConstantImages {
-  static String userProfile = 'assets/user_profiles/AMAL PK .jpg';
+  static String userProfile = 'assets/user_profiles/AMALPK.jpg';
   static String gMailIcon = 'assets/social_media_icons/gmail.png';
   static String linkedIcon = 'assets/social_media_icons/linkedin.png';
   static String gitHubIcon = 'assets/social_media_icons/github.png';
